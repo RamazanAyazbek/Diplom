bdaed157efc7234b6736fda8cd0c2f579dded52c
Ramazan <86975572+RamazanAyazbek@users.noreply.github.com>
2023-10-30 22:39:39 +0600

    Update settings.py

--- djangoregistertemplate/settings.py
+++ djangoregistertemplate/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER = 'maksplank351@gmail.com'
-# EMAIL_HOST_PASSWORD = 'fajeyeggzhwcajrt'
+# EMAIL_HOST_PASSWORD = 'herepasswordfromgoogle'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
